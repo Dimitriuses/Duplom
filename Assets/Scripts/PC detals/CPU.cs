affd4be8d8abc907e8d7ccb6142fb9d369819ef8
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2020-07-01 12:36:13 +0300

    start devices scripting

---
+++ Assets/Scripts/PC detals/CPU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.PC_detals
+{
+    public class CPU
+    {
+        char[] Socet;
+        float BaseFerequency;
+        float MaxFerequency;
+        int NumberOfCores;
+        int HeatOutput;
+        public bool withGPU;
+        public bool Broken;
+        public CPU()
+        {
+            Socet = new char[10];
+            Broken = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PC detals/MotherCard.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.PC_detals;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class MotherCard : MonoBehaviour
+{
+    int PCISize,PCIExpresSize, RAMSize, SATASize;
+    char[] Socet;
+    public CPU CPU { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Socet = new char[10];
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
