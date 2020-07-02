a22f54da22edeb3c2c3b145b8c5a5dae071bd624
Saintmember <saintmember@ukr.net>
2020-07-02 04:08:11 +0300

    Buttons

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class StartGame : MonoBehaviour
+{
+    void Start()
+    { 
+    }
+    void Update()
+    {  
+    }
+  public void OnStartGameClick()
+    {     
+        SceneManager.LoadScene("Game");
+    }
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }
+}
