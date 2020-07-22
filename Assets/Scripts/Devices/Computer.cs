f42bcd2b0765c2fb7701c16a4551ff56762da50e
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2020-07-22 20:37:46 +0300

    Sinus cosinus fix and add animator

--- Assets/Scripts/Devices/Computer.cs
+++ Assets/Scripts/Devices/Computer.cs
@@ -10,19 +10,21 @@ public class Computer : ShemObj
 {
     [Header("PCButtons")]
     public GameObject Power;
-    public UnityEngine.UI.Button[] UserB;
+    public RectTransform[] UserB;
     public Sprite[] PowerBMat;
     public CircleCollider2D UseColiider;
     public CircleCollider2D EditCollider;
     public int RValue;
     public float PValue;
     public float Radius;
+    public float OValue;
 
     [Header("Computer")]
     public Sprite[] PCMaterials;
     
     //bool isMCARD, isCPU, isGPU, isRAM, isSATA;
     MotherCard MotherCard;
+    Animator Animator;
 
     bool isPowerOn;
     bool isPowerOpen;
@@ -30,6 +32,11 @@ public class Computer : ShemObj
 
     int networkSocets;
     int electricalSocets;
+
+    //int Aop;
+    //int Acl;
+    //int Ajp;
+    //int Adv;
     private void Start()
     {
         base.Start();
@@ -37,6 +44,11 @@ public class Computer : ShemObj
         base._OnMouseDown = OnMD;
         base._OnMouseExit = OnMExit;
         base._OnMouseUp = OnMUP;
+        Animator = GetComponent<Animator>();
+        //Aop = Animator.StringToHash("openingUserButtons");
+        //Acl = Animator.StringToHash("closingUserButtons");
+        //Ajp = Animator.StringToHash("Jamping");
+        //Adv = Animator.StringToHash("Diving");
         //Debug.Log("Start Computer");
         //Debug.Log(Power.GetComponent<Button>().onClick.ToString());
         //Power.GetComponent<Button>().onClick.AddListener(PowerTrigered);
@@ -51,6 +63,7 @@ public class Computer : ShemObj
         isPowerOn = !isPowerOn;
         //Debug.Log("Power" + isPowerOn);
         RevizPowerSprite();
+        
     }
 
     void RevizPowerSprite()
@@ -59,11 +72,13 @@ public class Computer : ShemObj
         {
             Power.GetComponent<SpriteRenderer>().sprite = PowerBMat[0];
             GetComponent<SpriteRenderer>().sprite = PCMaterials[0];
+            Animator.SetTrigger("Open");
         }
         else
         {
             Power.GetComponent<SpriteRenderer>().sprite = PowerBMat[1];
             GetComponent<SpriteRenderer>().sprite = PCMaterials[1];
+            Animator.SetTrigger("Close");
         }
     }
 
@@ -80,6 +95,7 @@ public class Computer : ShemObj
             Power.GetComponent<Animator>().SetBool("isOpen", true);
             //Power.GetComponent<Animator>();
             isPowerOpen = true;
+            Animator.SetTrigger("Jump");
         }
 
     }
@@ -95,6 +111,7 @@ public class Computer : ShemObj
         {
             Power.GetComponent<Animator>().SetBool("isOpen", false);
             isPowerOpen = false;
+            Animator.SetTrigger("Drive");
         }
     }
 
@@ -103,27 +120,38 @@ public class Computer : ShemObj
 
     }
 
-    void ButtonPosChange(UnityEngine.UI.Button button, float rotateVaule)
+    Vector3 ButtonPosChange(/*RectTransform button,*/ float rotateVaule)
     {
         float ammount = (PValue / 100) * rotateVaule / RValue;
         float BRV = ammount * 360;
-        button.GetComponent<RectTransform>().position = new Vector3((float)Math.Cos(BRV)*Radius, (float)Math.Sin(BRV)*Radius, 0);
-        Debug.Log(BRV + " cos=" + Math.Cos(BRV) + "; sin=" + ConvertRadiansToDegrees(Math.Sin(BRV)));
+        double BRVD = ConvertRadiansToDegrees(BRV);
+        //button.anchoredPosition = new Vector3((float)Math.Cos(BRVD)*Radius, (float)Math.Sin(BRVD)*Radius, 0);
+        return new Vector3((float)Math.Cos(BRVD) * Radius, (float)Math.Sin(BRVD) * Radius, 0);
+        //Debug.Log(BRV +"xDEG="+ ConvertRadiansToDegrees(BRV) + " cos=" + Math.Cos(ConvertRadiansToDegrees(BRV)) + "; sin=" + Math.Sin(ConvertRadiansToDegrees(BRV)));
+    }
+
+    Vector3 ButtonPosMove(Vector3 vector3)
+    {
+        return new Vector3((vector3.x/100)*OValue, (vector3.y/100)*OValue, (vector3.z/100)*OValue);
     }
 
     private void Update()
     {
+
         int max = UserB.Length; 
         for (int i = 0; i < max; i++)
         {
-            ButtonPosChange(UserB[i], RValue / max * (i+1));
+            //ButtonPosChange(UserB[i], RValue / max * (i+1));
+            UserB[i].anchoredPosition = ButtonPosMove(ButtonPosChange(RValue / max * (i + 1)));
         }
+        
     }
 
 
     public static double ConvertRadiansToDegrees(double radians)
     {
-        double degrees = (180 / Math.PI) * radians;
+        //double degrees = (180 / Math.PI) * radians;
+        double degrees = Math.PI * radians / 180.0;
         return (degrees);
     }
 }
