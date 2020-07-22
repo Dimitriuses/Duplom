01b43f9c4096512a23ebe25040a64560c2c86d17
shpacosky.dmutro.1999.mc <shpacosky.dmutro.1999.mc@gmail.com>
2020-07-22 11:32:12 +0300

    Create class Saving

---
+++ Assets/Scripts/Saving.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.WSA;
+
+
+//[CreateAssetMenu(menuName = "Save")]
+public class Saving//: ISerializationCallbackReceiver
+{
+    public Folder Path;// => _path;
+    public string Name;// => _name;
+    public DateTime Date;// => _date;
+
+    //[SerializeField] private Folder _path;
+    //[SerializeField] private string _name;
+    //[SerializeField] private DateTime _date;
+}
+
