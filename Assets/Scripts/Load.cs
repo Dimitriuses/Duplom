33fd13eebbfc88b7e727683516ae20ff2d4d4a70
Saintmember <saintmember@ukr.net>
2020-07-29 02:36:05 +0300

    Loading

---
+++ Assets/Scripts/Load.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+    //Start is called before the first frame update
+    void Start()
+    {
+        List<string> listOfSaves = new List<string>()
+{
+    "John Doe",
+    "Jane Doe",
+    "Joe Doe"
+};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
