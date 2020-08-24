999c62b9eeef2b33ac5fd567bfd7456504e281dc
Saintmember <saintmember@ukr.net>
2020-08-24 23:29:48 +0300

    Add exit button.

--- Assets/Scripts/UI/UIResources.cs
+++ Assets/Scripts/UI/UIResources.cs
@@ -6,12 +6,10 @@ using Assets.Scripts.Resources;
 public class UIResources : MonoBehaviour
 {
     ResourcesStorage Resouces;
-
     void Start()
     {
-        
+       // Resources = new ResourcesStorage(TESTmoney, TESTelectricity, TESTnetwork, TESTexperience);
     }
-
 
     void Update()
     {
