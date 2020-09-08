326a5ac9d446e619ffea85d75aeb46f1b11026af
Saintmember <saintmember@ukr.net>
2020-09-08 23:12:48 +0300

    Script.

--- Assets/Scripts/UI/UIResources.cs
+++ Assets/Scripts/UI/UIResources.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Resources;
-
 public class UIResources : MonoBehaviour
 {
     ResourcesStorage Resouces;
+    //public List<ResourcesItem> GetDefaultResources()
+    //{
+    //    List<ResourcesItem> resources = new List<ResourcesItem>();
+    //    foreach (KeyValuePair<RID, string> item in NameToRIDTranslete)
+    //    {
+    //        resources.Add(Resouces.Find(x => x.Name == item.Value));
+    //    }
+    //    return resources;
+    //}
     void Start()
     {
        // Resources = new ResourcesStorage(TESTmoney, TESTelectricity, TESTnetwork, TESTexperience);
     }
-
     void Update()
     {
         
