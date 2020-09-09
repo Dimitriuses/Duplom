6c50ef081749db43175583ca20def46ff5a65589
Volodymyr Novak <novakvova@gmail.com>
2020-09-09 14:47:28 +0300

    start PlayerCursor (Dimitriuses in shag)

---
+++ Assets/Scripts/PlayerCursor.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Items;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+
+    public ITool Tool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Tool.onClick();
+    }
+
+}
